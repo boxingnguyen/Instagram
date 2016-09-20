6d73292f2845dc2f99d0b15820ca95df6ead31de
nguyenvietduc <ducnv@tmh-techlab.vn>
2016-09-20 11:55:24 +0700

    Complete top page

--- app/Console/Command/CalculateReactionShell.php
+++ app/Console/Command/CalculateReactionShell.php
@@ -18,42 +18,51 @@ class CalculateReactionShell extends AppShell {
 				array('$match' => array('date_add_to_mongo' => $date_now)),
 				array(
 					'$group' => array(
-						'_id' => '$id',
+						'_id' => '$user.id',
 						'username' => array('$first' => '$username'),
+						'fullname' => array('$first' => '$full_name'),
 						'followers' => array('$first' => '$followed_by.count'),
 						'media_count' => array('$first' => '$media.count')
 					)
 				)
-				
 		);
 		$data = $collection->aggregate($condition);
-		// split accounts into chunks
-		$chunks = array_chunk($data['result'], 5);
-				
-		foreach ($chunks as $account_list) {
-			foreach ($account_list as $key => $value) {
-				// create 2 processes here
-				$pid = pcntl_fork();
-		
-				if ($pid == -1) {
-					die('could not fork');
-				} else if ($pid) {
-					// we are the parent
-					// collect process id to know when children complete
-					$pids[] = $pid;
-				} else {
-					// we are the child
-					$data['result'][$key]['likes'] = $this->__calculateReaction($value['_id'])['likes'];
-					$data['result'][$key]['comments'] = $this->__calculateReaction($value['_id'])['comments'];
-					exit;
-				}
-			}
-			foreach ($pids as $pid) {
-				pcntl_waitpid($pid, $status);
-				unset($pids[$pid]);
-			}
+		if (empty($data['result'])) {
+			$newdate = date('d M Y', strtotime('-1 day', strtotime($date_now)));
+			$condition = array(
+					array('$match' => array('date_add_to_mongo' => $newdate)),
+					array(
+							'$group' => array(
+									'_id' => '$id',
+									'username' => array('$first' => '$username'),
+									'fullname' => array('$first' => '$full_name'),
+									'followers' => array('$first' => '$followed_by.count'),
+									'media_count' => array('$first' => '$media.count')
+							)
+					)
+			);
+			$data = $collection->aggregate($condition);
 		}
-		print_r($data['result']);
+		$time1 = microtime(true);
+		$count = 1;
+		$result = $data['result'];
+		foreach ($data['result'] as $key => $value) {
+			$reaction = $this->__calculateReaction($value['_id']);
+			$result[$key]['likes'] = $reaction['likes'];
+			$result[$key]['comments'] = $reaction['comments'];
+			echo $count . ". " . $value['username'] . " completed!" . PHP_EOL;
+			$count ++;
+		}
+		
+		// write result (after calculating reaction) into database
+		$db = $m->instagram_account_info;
+		$collection = $db->reaction;
+		$collection->drop();
+		print_r($result);
+		$collection->batchInsert($result);
+		
+		$time2 = microtime(true);
+		echo "Took: " . ($time2 - $time1) . PHP_EOL;
 	}
 	
  	private function __calculateReaction($account_id) {
@@ -62,7 +71,6 @@ class CalculateReactionShell extends AppShell {
 				array(
 						'$group' => array(
 								'_id' => '$user.id',
-								'username' => array('$first' => '$user.username'),
 								'total_likes' => array('$sum' => '$likes.count'),
 								'total_comments' => array('$sum' => '$comments.count'),
 						)
@@ -73,5 +81,5 @@ class CalculateReactionShell extends AppShell {
 		$result['likes'] = $data['result'][0]['total_likes'];
 		$result['comments'] = $data['result'][0]['total_comments'];
 		return $result;
-	}	
+	}
 }
--- app/Controller/TopController.php
+++ app/Controller/TopController.php
@@ -1,31 +1,10 @@
 <?php
 class TopController extends AppController {
-	public $mongoCursor;
-	
-	public function beforeFilter() {
-		$m = new MongoClient();
-		$db = $m->instagram;
-		$this->mongoCursor = $db->media;
-	}
-	
 	public function index () {
 		$m = new MongoClient();
 		$db = $m->instagram_account_info;
-		$collection = $db->account_info;
-		
-		$date_now = date('d M Y');
-		$condition = array(
-				array('$match' => array('date_add_to_mongo' => $date_now)),
-				array(
-					'$group' => array(
-						'_id' => '$id',
-						'username' => array('$first' => '$username'),
-						'followers' => array('$first' => '$followed_by.count'),
-						'media_count' => array('$first' => '$media.count')
-					)
-				)
-				
-		);
-		$data = $collection->aggregate($condition);
+		$collection = $db->reaction;
+		$data = $collection->find()->sort(array('followers' => -1));
+		$this->set(compact('data'));
 	}
 }
