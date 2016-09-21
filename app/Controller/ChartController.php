e3c1256f1ba6f5ac6946ebc399b44b7ff705dcd2
nhinhdt <nhinhdt@tmh-techlab.vn>
2016-09-21 15:56:26 +0700

    edit like chart

--- app/Controller/ChartController.php
+++ app/Controller/ChartController.php
@@ -34,21 +34,69 @@ class ChartController extends AppController {
 		}
 		$this->set('dataComments', $arrComment);
 	}
+// 	public function like() {
+// 		$m = new MongoClient();
+// 		$dbChart = $m->chart;
+// 		$cllChart = $dbChart->selectCollection(date('Y_m'));
+// 		$id = $this->request->query['id'];
+// 		$arrLikes = array();
+// 		if($id) {
+// 			$data = $cllChart->find(array('accuntID' => $id));
+// 			if (isset($data) && $data->count() > 0) {
+// 				foreach ($data as $vlike) {
+// 					$arrLikes[$vlike['time']] = $vlike['likes'];
+// 				}
+// 			}
+// 		}
+// 		$this->set('dataLikes', $arrLikes);
+// 	}
 	public function like() {
+		$this->layout = false;
+		$this->autoRender = false;
 		$m = new MongoClient();
-		$dbChart = $m->chart;
-		$cllChart = $dbChart->selectCollection(date('Y_m'));
-		$id = $this->request->query['id'];
-		$arrLikes = array();
-		if($id) {
-			$data = $cllChart->find(array('accuntID' => $id));
-			if (isset($data) && $data->count() > 0) {
-				foreach ($data as $vlike) {
-					$arrLikes[$vlike['time']] = $vlike['likes'];
+		$dbChart = $m->instagram;
+		$cllChart = $dbChart->media;
+		
+		
+		
+		$month = 9;
+		$m = new MongoClient();
+		$db = $m->instagram;
+		$collection = $db->media;
+		
+		$id = $this->params->query['id'];
+// 		$id = '18428658';
+		date_default_timezone_set("UTC");
+		
+		$numberDaysPerMonth = cal_days_in_month(CAL_GREGORIAN, $month, 2016);
+		$analytic = array();
+		$fileds = array('created_time'=>1,'user.username'=>1,'likes.count'=>1,'comments.count'=>1);
+		for ($day = 1 ; $day <= $numberDaysPerMonth ; $day++){
+			$start = strtotime("2016-$month-$day 00:00:00");
+			$end = strtotime("2016-$month-$day 23:59:59");
+			$query = array(
+					'created_time' => array(
+							'$gte' => "$start",
+							'$lte' => "$end"
+					),
+					'user.id' => $id
+			);
+			$cursor = $collection->find($query, $fileds);
+			$count = 0;
+			if(!empty($cursor)){
+				foreach ($cursor as $v){
+					$count += $v['likes']['count'];
+					// 					$count += $v['comments']['count'];
 				}
 			}
+			$analytic[$month.'/'.$day]= $count;
+			
 		}
-		$this->set('dataLikes', $arrLikes);
+		$this->set('dataLikes', $analytic);
+// 		echo "<pre>";
+// 		print_r($analytic);
+		
+		
 	}
 }
 
