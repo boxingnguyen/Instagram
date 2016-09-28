b09110590920f923ac3860fe12a7a8a6391cad34
nhinhdt <nhinhdt@tmh-techlab.vn>
2016-09-28 10:30:32 +0700

    edit break in while

--- app/Console/Command/GetMediaThreeMonthShell.php
+++ app/Console/Command/GetMediaThreeMonthShell.php
@@ -10,7 +10,7 @@ class GetMediaThreeMonthShell extends AppShell {
 		//name file
 		date_default_timezone_set('UTC');
 		$nameFile = date('dmY');
-		$myfile = fopen("/www/htdocs/PHPInstagram/app/Vendor/Data/".$nameFile.".media.json", "w") or die("Unable to open file!");
+		$myfile = fopen("/www/htdocs/PHPInstagram/app/Vendor/Data/".$nameFile.".media.json", "w+") or die("Unable to open file!");
 		//find date 3 month ago
 		$fineStamp = date('Y-m-d 00:00:00');
 		$d = new DateTime($fineStamp);
@@ -18,11 +18,9 @@ class GetMediaThreeMonthShell extends AppShell {
 		$time = strtotime($d->format('Y-m-d 00:00:00'));
 		$this->out($time);
 		$arrData = array();
-		
 		if(isset($all_account) && !empty($all_account)) {
 			foreach ($all_account as $username) {
 				$i = 0;
-		
 				$max_id = null;
 				do {
 					if(isset($max_id) && $max_id != null) {
@@ -33,22 +31,21 @@ class GetMediaThreeMonthShell extends AppShell {
 					if(isset($data->items) && !empty($data->items)) {
 						$max_id = end($data->items)->id;
 						$maxTime = end($data->items)->created_time;
-						echo "Max id: ".$i.' '.$max_id.PHP_EOL;
-						$i++;
 						if($maxTime < $time) {
-							$this->out($maxTime);
+							$this->out('Time post: '.$maxTime);
 							foreach ($data->items as $val) {
 								if($val->created_time >= $time) {
 									fwrite($myfile, json_encode($val).PHP_EOL);
 								}
 							}
-							$data->more_available = false;
+							break;
 						} else {
 							foreach($data->items as $val){
 								fwrite($myfile, json_encode($val).PHP_EOL);
 							}
 						}
-		
+						echo "Max id: ".$i.' '.$max_id.PHP_EOL;
+						$i++;
 					}
 				} while (isset($data->more_available) && ($data->more_available == true || $data->more_available == 1));
 				echo "Complete username: ".$username.PHP_EOL;
