beda231b9378f52e1a17ac0600d411265e73c13a
nguyenvietduc <ducnv@tmh-techlab.vn>
2016-09-28 16:46:40 +0700

    chmod to read/write to tmp/ folder

--- app/Controller/TopController.php
+++ app/Controller/TopController.php
@@ -1,6 +1,7 @@
 <?php
 class TopController extends AppController {
 	public function index () {
+		exec('sudo chmod -R 777 /www/app/tmp');
 		$m = new MongoClient();
 		$db = $m->instagram_account_info;
 		$collection = $db->reaction;
