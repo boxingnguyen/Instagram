c59a779d67752f311ffed412999e6d1f38f89894
hangktt <hangktt@tmh-techlab.vn>
2016-09-30 14:55:38 +0700

    edit register.js

--- app/webroot/js/src/register.js
+++ app/webroot/js/src/register.js
@@ -28,41 +28,41 @@ $().ready(function(){
 			username = username.slice(0, -1);
 		}
 		console.log(username);
-	});
-	$('button.modalReg').click(function(){
-		var username = $('#inputUserName').val();
-		if($('input[type=text]').val() == ''){
-			$('p.message').text('Please input the link of instagram!').css("color",'red');
-			$('button.modalReg').prop('disabled', true);
-		}else{
-			$('#myModal').modal('hide');
-			$('.loader').show();
-			$('.modal-body').css('opacity','0.4');
-			$.ajax({
-				method: "POST",
-				url: '/check',
-				data: {username:username},
-				dataType: 'json',
-				success: function(data){
-					if(data == 1){
-						$('p.messRegis').text('You have successfully registered');
-					}else{
-						if($.type(data) === "string"){
-							$('p.messRegis').text(data);
+		$('button.modalReg').click(function(){
+			if($('input[type=text]').val() == ''){
+				$('p.message').text('Please input the link of instagram!').css("color",'red');
+				$('button.modalReg').prop('disabled', true);
+			}else{
+				$('#myModal').modal('hide');
+				$('.loader').show();
+				$('.modal-body').css('opacity','0.4');
+				$.ajax({
+					method: "POST",
+					url: '/check',
+					data: {username:username},
+					dataType: 'json',
+					success: function(data){
+						if(data == 1){
+							$('p.messRegis').text('You have successfully registered');
+						}else{
+							if($.type(data) === "string"){
+								$('p.messRegis').text(data);
+							}
 						}
+						$('.loader').hide();
+						$('.modal-body').css('opacity','1');
+					},
+					error: function(){
+						$('.loader').hide();
+						$('.modal-body').css('opacity','1');
+						$('p.messRegis').text('Sorry, Ajax has some problem!');
 					}
-					$('.loader').hide();
-					$('.modal-body').css('opacity','1');
-				},
-				error: function(){
-					$('.loader').hide();
-					$('.modal-body').css('opacity','1');
-					$('p.messRegis').text('Sorry, Ajax has some problem!');
-				}
-			});
-		}
-		
+				});
+			}
+			
+		});
 	});
+	
 	$('button.cancel').click(function(){
 		$('#regisForm').modal('hide');
 		$('.modal-backdrop, .modal-backdrop.fade.in').css('opacity',"0");
