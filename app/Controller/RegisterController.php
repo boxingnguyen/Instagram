eda6093f55591a7691c0b8cef6c4b1241a8a20e5
hangktt <hangktt@tmh-techlab.vn>
2016-10-12 15:08:12 +0700

    button cancel when login

--- app/Controller/RegisterController.php
+++ app/Controller/RegisterController.php
@@ -7,24 +7,28 @@ class RegisterController extends AppController {
 		$this->set('instagrams', $url);
 	}
 	public function detail() {
-		$m = new MongoClient;
-		$db = $m->instagram_account_info;
-		$collections = $db->account_username;
-		
-		$code = $_GET['code'];
-		$data = $this->_instagram->getOAuthToken($code);
-		$id = $data->user->id;
-		print_r($data);
-		$setId = $collections->find(array('id' => $id))->count();
-		if ($setId > 0) {
-			$collections->remove(array('id' => $id));
-		} 
-		$collections->insert(array(
-				'access_token' => $data->access_token,
-				'id' => $id,
-				'username' => $data->user->username
-		));
-		$this->redirect( array('controller' => 'top','action' => 'index' ));
+		if(isset($_GET['code'])){
+			$m = new MongoClient;
+			$db = $m->instagram_account_info;
+			$collections = $db->account_username;
+			
+			$code = $_GET['code'];
+			$data = $this->_instagram->getOAuthToken($code);
+			$id = $data->user->id;
+			print_r($data);
+			$setId = $collections->find(array('id' => $id))->count();
+			if ($setId > 0) {
+				$collections->remove(array('id' => $id));
+				$collections->insert(array(
+						'access_token' => $data->access_token,
+						'id' => $id,
+						'username' => $data->user->username
+				));
+			}
+			$this->redirect( array('controller' => 'top','action' => 'index' ));
+		}else {
+			$this->redirect( array('controller' => 'register','action' => 'login' ));
+		}
 		
 	}
 
