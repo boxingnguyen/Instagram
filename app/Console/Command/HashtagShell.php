17da50a07e03414d96d398a167a87035f76bab38
nguyenvietduc <ducnv@tmh-techlab.vn>
2016-10-26 17:27:43 +0700

    drop Media (hashtag) before insert

--- app/Console/Command/HashtagShell.php
+++ app/Console/Command/HashtagShell.php
@@ -49,6 +49,7 @@ class HashtagShell extends AppShell {
 		$m = new MongoClient();
 		$db = $m->hashtag;
 		$c_media = $db->media;
+		$c_media->drop();
 
 		$date = date('dmY');
 		$hashtag = $this->__getHashtag();
@@ -94,6 +95,7 @@ class HashtagShell extends AppShell {
 							} else if (isset($value->date)) {
 								$value->date = date('d-m-Y', $value->date);
 							}
+							$value->tag_name = $tag;
 							fwrite($myfile, json_encode($value) . "\n");
 						}
 					}
