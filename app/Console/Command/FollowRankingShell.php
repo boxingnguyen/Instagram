ddce3cc71ea317615954609216b1ce046f7112ec
nhinhdt <nhinhdt@tmh-techlab.vn>
2016-11-02 14:03:51 +0700

    FollowRankingShell

--- app/Console/Command/FollowRankingShell.php
+++ app/Console/Command/FollowRankingShell.php
@@ -7,35 +7,21 @@ class FollowRankingShell extends AppShell {
 		$m = new MongoClient;
 		$db = $m->instagram;
 		$this->__collection = $db->follow;
-// 		$currentDate = (new DateTime())->modify('-1 day')->format('Y-m-d');
-// 		$time = date('Y-m', strtotime($currentDate));
-// 		$this->__collection = $db->selectCollection($time);
-// 		$this->__collection = $db->selectCollection('username'.$time);
-// 		$this->__collectionLogin = $db->selectCollection('login'.$time);
 	}
 	public function main() {
 		$mLogin = new MongoClient;		
 		$dbLogin = $mLogin->instagram_account_info;
 		$colLogin = $dbLogin->account_login;
 		$colUsername = $dbLogin->account_username;
-// 		$beforeTime = (new DateTime())->modify('-1 day')->format('Y-m-d');
-		//collection account_username
 		$listAccount = $colUsername->find(array('access_token' => array('$exists' => true)));
 		if($listAccount->count() > 0) {
-// 			$this->__userFollow($listAccount, $this->__collection);
 			$this->__userFollow($listAccount);
 			//collection account_login
 			foreach ($listAccount as $val) {
-				//check account exitst account_username ? "delete account_login" : "get info of account"
 				$data = $colLogin->find(array('id' => $val['id']));
 				if ($data->count() > 0) {
 					$colLogin->remove(array('id' => $val['id']));
 				}
-				//check account exitst loginDate ? "delete record" : "....."
-// 				$checkColLoginData = $this->__collectionLogin->find(array($val['id'] => array('$exists' => 1), 'time' => $beforeTime));
-// 				if ($checkColLoginData->count() > 0) {
-// 					$this->__collectionLogin->remove(array($val['id'] => array('$exists' => 1), 'time' => $beforeTime));
-// 				}
 			}
 			echo PHP_EOL.'Finish account_username'.PHP_EOL;
 		}
@@ -60,7 +46,6 @@ class FollowRankingShell extends AppShell {
 					$infoFollowsBy = $this->_insta->getUserFollower($cursor);
 				}
 				if(isset($infoFollowsBy) && !empty($infoFollowsBy->data)) {
-						
 					//get total follow each account
 					$dataFollow = $infoFollowsBy->data;
 					foreach ($dataFollow as $valFollow) {
@@ -81,7 +66,6 @@ class FollowRankingShell extends AppShell {
 								'full_name' => $valFollow->full_name, 'totalFollow' => $countFollowBy,
 								'follows' => $countFollows
 						);
-						// usort($arr, function($a, $b) { return $a['totalFollow'] < $b['totalFollow'] ? 1 : -1 ; } );
 					}
 						
 				} else {
@@ -104,10 +88,9 @@ class FollowRankingShell extends AppShell {
 	}
 	
 	private function __saveFollow($accountId, $arr) {
-// 		$beforeTime = (new DateTime())->modify('-1 day')->format('Y-m-d');
 		$userId = $this->__collection->find(array($accountId => array('$exists' => 1)));
 		if ($userId->count() > 0) {
-			$collection->remove(array($accountId => array('$exists' => 1)));
+			$this->__collection->remove(array($accountId => array('$exists' => 1)));
 		}
 		$this->__collection->insert(array($accountId => $arr));
 	}
